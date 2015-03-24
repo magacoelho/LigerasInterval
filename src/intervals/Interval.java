80ba3bd999980596e979a0bf5851b4572ffedab7
bl0819 <bl0819@192.168.7.1>
2015-03-24 13:42:07 +0100

    UPDATE Interval getters setters

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -13,6 +13,26 @@ public abstract  class Interval {
 			this.min = min;
 			this.max = max;
 		}
+    
+
+	public Double getMin() {
+		return min;
+	}
+
+
+	public void setMin(Double min) {
+		this.min = min;
+	}
+
+
+	public Double getMax() {
+		return max;
+	}
+
+
+	public void setMax(Double max) {
+		this.max = max;
+	}
 
 
 	public  double midPoint() {
--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -29,6 +29,7 @@ public class IntervalFactory {
 		default:
 			break;
 		}
+		
 	return interval;
 	}
 	
